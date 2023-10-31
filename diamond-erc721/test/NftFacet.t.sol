03db36f3159e1a0791fb52849e9ec11585889491
Esther Breath <estherojile248@gmail.com>
2023-10-31 22:52:35 +0100

    first commit

---
+++ diamond-erc721/test/NftFacet.t.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "./helpers/DiamondDeployer.sol";
+
+contract NFTTest is DiamondDeployer {
+    function testTokenName() public {
+        assertEq(nftC.name(), "JAyNFT");
+    }
+
+    function testTokenSymbol() public {
+        assertEq(nftC.symbol(), "JNFT");
+    }
+
+    function testMinting() public {
+        nftC.mint(msg.sender);
+        assertEq(nftC.ownerOf(0), msg.sender);
+        // assertEq(nftInt.tokenURI(0), "https://brknarsy.github.io/erc721-foundry/jsons/1.json");
+    }
+
+    function testBalances() public {
+        testMinting();
+        testMinting();
+        assertEq(nftC.balanceOf(msg.sender), 2);
+    }
+
+    function testInvalidAddress() public {
+        testMinting();
+        vm.expectRevert("ERC721: address zero is not a valid owner");
+        nftC.balanceOf((address(0)));
+    }
+
+    function testOwner() public {
+        testMinting();
+        assertEq(nftC.ownerOf(0), msg.sender);
+    }
+
+    function testApproval() public {
+        address user1 = vm.addr(1232);
+        testMinting();
+        vm.prank(msg.sender);
+        nftC.approve(user1, 0);
+        assertEq(nftC.getApproved(0), user1);
+    }
+
+    function testAllAproval() public {
+        address user1 = vm.addr(1232);
+        testMinting();
+        testMinting();
+        vm.prank(msg.sender);
+        nftC.setApprovalForAll(user1, true);
+
+        assertTrue(nftC.isApprovedForAll(msg.sender, user1));
+    }
+
+    function testTransferFrom() public {
+        address user1 = vm.addr(1232);
+        testMinting();
+        vm.prank(msg.sender);
+        nftC.setApprovalForAll(user1, true);
+        vm.prank(user1);
+        nftC.transferFrom(msg.sender, address(this), 0);
+        assertEq(nftC.ownerOf(0), address(this));
+    }
+
+    function testSafeTransferFrom() public {
+        address user1 = vm.addr(1232);
+        testMinting();
+        vm.prank(msg.sender);
+        nftC.setApprovalForAll(user1, true);
+        vm.prank(user1);
+        vm.expectRevert("ERC721: transfer to non ERC721Receiver implementer");
+        nftC.safeTransferFrom(msg.sender, address(this), 0);
+    }
+}
---
+++ diamond-erc721/test/helpers/DiamondDeployer.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "../../contracts/interfaces/IDiamondCut.sol";
+import "../../contracts/facets/DiamondCutFacet.sol";
+import "../../contracts/facets/DiamondLoupeFacet.sol";
+import "../../contracts/facets/OwnershipFacet.sol";
+import "../../contracts/Diamond.sol";
+import "../../contracts/facets/MarketPlaceFacet.sol";
+import "../../contracts/facets/NftFacet.sol";
+
+import "./DiamondUtils.sol";
+
+contract DiamondDeployer is DiamondUtils, IDiamondCut {
+    //contract types of facets to be deployed
+    Diamond diamond;
+    DiamondCutFacet dCutFacet;
+    DiamondLoupeFacet dLoupe;
+    OwnershipFacet ownerF;
+    NFTMarketPlace market;
+    NFTRC nft;
+    NFTRC nftC;
+    NFTMarketPlace marketF;
+
+    uint256 creatorPriv;
+    uint256 spenderPriv;
+
+    address creator;
+    address spender;
+
+    function setUp() public {
+        //deploy facets
+        dCutFacet = new DiamondCutFacet();
+        diamond = new Diamond(address(this), address(dCutFacet), "JAyNFT", "JNFT");
+        dLoupe = new DiamondLoupeFacet();
+        ownerF = new OwnershipFacet();
+        market = new NFTMarketPlace();
+        nft = new NFTRC();
+        nftC = NFTRC(address(diamond));
+        marketF = NFTMarketPlace(address(diamond));
+
+        (creator, creatorPriv) = mkaddr("CREATOR");
+        (spender, spenderPriv) = mkaddr("SPENDER");
+
+        //upgrade diamond with facets
+
+        //build cut struct
+        FacetCut[] memory cut = new FacetCut[](4);
+
+        cut[0] = (
+            FacetCut({
+                facetAddress: address(dLoupe),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("DiamondLoupeFacet")
+            })
+        );
+
+        cut[1] = (
+            FacetCut({
+                facetAddress: address(ownerF),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("OwnershipFacet")
+            })
+        );
+
+        cut[2] = (
+            FacetCut({
+                facetAddress: address(market),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("NFTMarketPlace")
+            })
+        );
+
+        cut[3] = (
+            FacetCut({
+                facetAddress: address(nft),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("NFTRC")
+            })
+        );
+
+        //upgrade diamond
+        IDiamondCut(address(diamond)).diamondCut(cut, address(0x0), "");
+
+        //call a function
+        DiamondLoupeFacet(address(diamond)).facetAddresses();
+    }
+
+    function diamondCut(FacetCut[] calldata _diamondCut, address _init, bytes calldata _calldata) external override {}
+
+    function mkaddr(string memory name) public returns (address addr, uint256 privateKey) {
+        privateKey = uint256(keccak256(abi.encodePacked(name)));
+        addr = vm.addr(privateKey);
+        vm.label(addr, name);
+    }
+
+    function switchSigner(address _newSigner) public {
+        vm.startPrank(_newSigner);
+        vm.deal(_newSigner, 3 ether);
+        vm.label(_newSigner, "USER");
+    }
+}
